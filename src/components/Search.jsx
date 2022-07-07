b35f0fc7a2001b8d957477c676767f320214b619
Emily Menezes <estudosmenezes@gmail.com>
2022-07-07 19:45:11 -0300

    Corrigindo req 2 e req 5

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ProductCard from './ProductCard';
 import { getProductsFromCategoryAndQuery } from '../services/api';
 
@@ -24,47 +25,56 @@ class Search extends Component {
     this.setState({ products: data, setSearch: true });
   }
 
-  render() {
-    const { products, table, setSearch } = this.state;
-    const flagResult = setSearch
-      ? <div>Nenhum produto foi encontrado</div>
-      : (
-        <div
-          data-testid="home-initial-message"
-        >
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </div>);
-    return (
-      <div>
-        <input
-          type="text"
-          data-testid="query-input"
-          onChange={ this.onInputChange }
-          value={ table }
-        />
-        <button
-          type="button"
-          data-testid="query-button"
-          onClick={ this.onHandleClick }
-        >
-          Search
-        </button>
-        {products.length === 0
-          ? (flagResult)
-          : (
-            <div>
-              {products.map(({ id, title, price, thumbnail }) => (
-                <ProductCard
-                  key={ id }
-                  title={ title }
-                  price={ price }
-                  thumbnail={ thumbnail }
-                />))}
-            </div>
-          )}
-      </div>
-    );
-  }
+   handleCartButton = () => {
+     const { history } = this.props;
+     history.push('/cart');
+   }
+
+   render() {
+     const { products, table, setSearch } = this.state;
+     const flagResult = setSearch
+       ? <div>Nenhum produto foi encontrado</div>
+       : (
+         <div
+           data-testid="home-initial-message"
+         >
+           Digite algum termo de pesquisa ou escolha uma categoria.
+         </div>);
+     return (
+       <div>
+         <input
+           type="text"
+           data-testid="query-input"
+           onChange={ this.onInputChange }
+           value={ table }
+         />
+         <button
+           type="button"
+           data-testid="query-button"
+           onClick={ this.onHandleClick }
+         >
+           Search
+         </button>
+         {products.length === 0
+           ? (flagResult)
+           : (
+             <div>
+               {products.map(({ id, title, price, thumbnail }) => (
+                 <ProductCard
+                   key={ id }
+                   title={ title }
+                   price={ price }
+                   thumbnail={ thumbnail }
+                 />))}
+             </div>
+           )}
+       </div>
+     );
+   }
 }
 
 export default Search;
+
+Search.propTypes = {
+  history: PropTypes.string.isRequired,
+};
