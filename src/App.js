1567c3e3ee73afbb172aca8c4cb8ee2592394b79
lucaseduardolema <lucaseduardolema@gmail.com>
2022-07-08 17:55:51 -0300

    iniciando req 7

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Search from './components/Search';
+import ProductPage from './Pages/ProductPage';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Search } />
+          <Route exact path='/product/:id' component={ ProductPage } />
         </Switch>
       </BrowserRouter>
     );
